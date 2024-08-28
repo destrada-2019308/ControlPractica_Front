1b3696142c0c26a78527a315396df887d5bc5b4b
destrada-2019308 <destrada-2019308@kinal.edu.gt>
2024-08-28 10:20:37 -0600

    eliminé clientButton

--- src/Components/Client/Admin/NavBarAdmin.jsx
+++ src/Components/Client/Admin/NavBarAdmin.jsx
@@ -14,7 +14,7 @@ export const NavBarAdmin = () => {
       <a className="navbar-brand m-4 " href='/home/prCtrl/clientAdmin'>Practice Control</a>
       
       <div>
-      <Button href="/home/prCtrl/clientAdmin/editClient" style={{  color: '#fff', padding: '10px 20px', border: 'none', borderRadius: '5px' }} className="btn btn-dark m-4"> Client</Button>
+      
       <Button style={{ backgroundColor: '#DC3545 ', color: '#fff', padding: '10px 20px', border: 'none', borderRadius: '5px' }} className="btn m-4" onClick={logout} type="submit">Log Out</Button>
       </div>
         
